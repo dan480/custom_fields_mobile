a31990316f87d8fa915ffd2f7e90f6012e833355
dan480 <sak_2003@mail.ru>
2021-08-19 18:23:43 +0000

    Create file

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'Mobile_shop',
+    'version':'1.1',
+    'author':'A.Sakovich',
+    'summary':"Online mobile phone shop",
+    'sequence': 1,
+    'description':"Store created on Odoo v13",
+    'category':'Shop',
+    'website':'null',
+    'depends':['base']
+
+}
