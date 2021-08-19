de5c152a1bdda90703347f6cf20f6918f56acf0a
dan480 <sak_2003@mail.ru>
2021-08-20 01:16:57 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,12 @@
     'description':"Store created on Odoo v13",
     'category':'Shop',
     'website':'null',
-    'depends':['sale']
+    'depends':['sale'],
+    'data': [
+        'views/assets.xml',
+        'views/templates.xml',
+        'views/sale_views.xml',
+        'wizard/sale_product_configurator_views.xml',
+    ],
 
 }
