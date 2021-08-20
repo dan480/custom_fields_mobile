d8bfbe48975e41c9bec1dcdfc81fc54c00d418c6
dan480 <sak_2003@mail.ru>
2021-08-20 19:16:51 +0300

    Update mobile_product_template.py

--- models/mobile_product_template.py
+++ models/mobile_product_template.py
@@ -8,5 +8,6 @@ class ProductMobile(models.Model):
     
     # custom fields
     manufacturer = fields.Selection([('huawei', 'Huawei'), ('xiaomi', 'Xiaomi'), ('samsung', 'Samsung'), ('sony', 'Sony')], string='Manufacturer')
-    manufacturer_id = fields.Many2one('manufacturer', 'Manufacturer Id', index=True, ondelete='cascade')
-    model = fields.One2many('product.template', 'manufacturer_id', 'Model')
+    manufacturer_id = fields.Many2one("manufacturer")
+    model_list = {'huawei':['p20', 'p20 pro', 'p30', 'p30 pro']}     
+    model = fields.One2many('model_list.values()', 'manufacturer_id', 'Model')
