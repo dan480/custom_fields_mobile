4288ee65002d9e45689ac3a07c318a92a9903b80
dan480 <sak_2003@mail.ru>
2021-08-20 16:09:01 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
-
 {
     'name':'Mobile_product',
     'version':'1.1',
     'author':'A.Sakovich',
     'summary':"Custom_felds_product",
-    'sequence': 1,
     'description':"Adds a custom field to the product",
-    'category':'service',
-    'website':'null',
+    'category':'Category',
     'depends':['base'],
     'data': ['views/product_template_views.xml'],
 
