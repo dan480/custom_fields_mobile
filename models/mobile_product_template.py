0e14a20091aeb2036804aa0ec8db15dd081eb980
dan480 <sak_2003@mail.ru>
2021-08-20 16:40:27 +0300

    Update mobile_product_template.py

--- models/mobile_product_template.py
+++ models/mobile_product_template.py
@@ -3,10 +3,8 @@
 from odoo import fields, models
 
 class ProductMobile(models.Model):
-    _name = "product.mobile"
     _inherit = "product.template"
     _description = "Add custom fields in Product Template"
-    _parent_store = True
     
     # custom fields
     manufacturer = fields.Selection([('huawei', 'Huawei'), ('xiaomi', 'Xiaomi'), ('samsung', 'Samsung'), ('sony', 'Sony')], string='Manufacturer')
