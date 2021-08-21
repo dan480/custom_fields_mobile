46927751f0ced15d87c8a33bcf75243ecfa1d967
dan480 <sak_2003@mail.ru>
2021-08-22 01:21:36 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -4,7 +4,7 @@ from odoo import fields, models, api
 
 class MobilePhone(models.Model):
     _name = "mobile.phone"
-    _inherit = "product.tmplate"
+    _inherit = "product.template"
     
     # Relations fields    
     manufacturer_id = fields.Many2one('manufacturer.phone', string='Manufacturer')
@@ -12,13 +12,13 @@ class MobilePhone(models.Model):
     
 class ManufacturerPhone(models.Model):
     _name = "manufacturer.phone"
-    _inherit = "product.tmplate"
+    _inherit = "product.template"
  
     manufacturer = fields.Char('Manufacturer')
     
 class ModelPhone(models.Model):
     _name = "model.phone"
-    _inherit = "product.tmplate"
+    _inherit = "product.template"
  
     model = fields.Char('Model')
     
