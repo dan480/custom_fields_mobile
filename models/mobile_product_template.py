47ad118a3af1430adff912478e31588dabe2eea8
dan480 <sak_2003@mail.ru>
2021-08-21 22:25:48 +0300

    Update mobile_product_template.py

--- models/mobile_product_template.py
+++ models/mobile_product_template.py
@@ -2,8 +2,8 @@
 
 from odoo import fields, models, api
 
-class ProductMobile(models.Model):
-    _name = "product.mobile"
+class MobilePhone(models.Model):
+    _name = "mobile.phone"
     
     # Relations fields    
     manufacturer_id = fields.Many2one('manufacturer.phone', string='Manufacturer')
