67b640203b15eb1ec9af3789090b786c74b05f3c
dan480 <sak_2003@mail.ru>
2021-08-22 02:40:57 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -6,8 +6,7 @@ class MobilePhone(models.Model):
     _inherit = "product.template"
     
     # Relations fields    
-    manufacturer_id = fields.Char('Manufacturer')
-    #model_id = fields.One2many('model_phone', 'manufacturer_id', string='Model')
-    manufacturer = fields.Char('Manufacturer')
-    model = fields.Char('Model')
+    
+    manufacturer = fields.Char()
+    model = fields.Char()
     
