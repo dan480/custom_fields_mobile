21306ce2ac88f5bd89f7de8d50ba29c3c66cceb0
dan480 <sak_2003@mail.ru>
2021-08-21 09:25:23 +0300

    Update mobile_product_template.py

--- models/mobile_product_template.py
+++ models/mobile_product_template.py
@@ -8,4 +8,4 @@ class ProductMobile(models.Model):
     
     # custom fields
     manufacturer = fields.Selection([('huawei', 'Huawei'), ('xiaomi', 'Xiaomi'), ('samsung', 'Samsung'), ('sony', 'Sony')], string='Manufacturer')
-    model_phone = fields.Selection([('p20',), ('p20 pro',), ('p30',), ('p30 pro',)], string='Model')
+    model = fields.Selection([('p20',), ('p20 pro',), ('p30',), ('p30 pro',)], string='Model')
