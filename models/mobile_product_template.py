4531055c2baafed3c356d2f8bf4bf9cc92ddd16e
dan480 <sak_2003@mail.ru>
2021-08-21 18:26:22 +0300

    Update mobile_product_template.py

--- models/mobile_product_template.py
+++ models/mobile_product_template.py
@@ -3,15 +3,20 @@
 from odoo import fields, models
 
 class ProductMobile(models.Model):
-    _inherit = "product.template"
+    _name = "product.mobile"
     _description = "Add custom fields in Product Template"
     
-    # custom field
-    manufacturer_list = [('huawei', 'Huawei'),
-                         ('sony', 'Sony')]
-    manufacturer = fields.Selection(selection_add(manufacturer_list),
-                                    index=True,
-                                    required=True)
+    # Relations fields    
+    manufacturer_id = fields.Many2one('manufacturer.phone', string='Manufacturer')
+    model_id = fields.One2many('model.phone', 'manufacturer_id', string='Model')
     
-    model_phone = fields.Selection([('p20', 'p20'), ('p20pro', 'p20pro')], stored=True, index=True, required=True)
+class ManufacturerPhone(models.Model):
+    _name = 'manufacturer.phone'
+ 
+    manufacturer = fields.Char('Manufacturer')
+    
+class ModelPhone(models.Model):
+    _name = 'model.phone'
+ 
+    model = fields.Char('Model')
     
