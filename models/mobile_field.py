a81efaf1119cf73ecc71f4151d31df6c50ccbea1
dan480 <sak_2003@mail.ru>
2021-08-22 11:21:43 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -5,7 +5,7 @@ class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
 
     #manufacturer_list = [('huawei', 'Huawei'), ('sony', 'Sony')]
-    manufacturer = fields.Selection([('huawei', 'Huawei'), ('sony', 'Sony')])
+    manufacturer = fields.Selection([('huawei', 'Huawei'), ('sony', 'Sony')], string="Manufacturer")
     #model_list = {'huawei': [('p20', 'p20'), ('p30', 'p30')]}
     #model = fields.Selection(model_list.get(manufacturer))
 
