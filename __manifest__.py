a3590f67eab1d638e544a601980a7e0bdb0749eb
dan480 <sak_2003@mail.ru>
2021-08-22 10:10:46 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -7,11 +7,12 @@
     'summary':"Custom mobile module",
     'description':"Module for creating a product-mobile phone",
     'category':'Customizations',
-    'depends':['base', 'sale', 'product'],
+    'depends':['product'],
     'data': [
         'views/mobile_views.xml',
         'views/add_phone_menu.xml',
         'views/add_quick_create.xml'
     ],
+    'installable':
 
 }
