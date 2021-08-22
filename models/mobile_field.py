c51dbb19c7fe63e08881f08f399336fae2b964a2
dan480 <sak_2003@mail.ru>
2021-08-22 09:53:39 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-
 from odoo import fields, models, api
 
-class MobilePhone(models.Model):
-    _name = "mobile.phone"
-    
+class ProductTemplateInherit(models.Model):
+    _inherit = "product.template"
+
     manufacturer_list = [('huawei', 'Huawei'), ('sony', 'Sony')]
     manufacturer = fields.Selection(manufacturer_list)
     model_list = {'huawei': [('p20', 'p20'), ('p30', 'p30')]}
     model = fields.Selection(model_list.get(manufacturer))
+
     
+
