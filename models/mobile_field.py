08aefbf44f144d8609b98816a170023798b344b5
dan480 <sak_2003@mail.ru>
2021-08-23 05:13:09 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
+
 
 class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
 
-    #manufacturer_list = [('huawei', 'Huawei'), ('sony', 'Sony')]
-    manufacturer = fields.Selection([('huawei', 'Huawei'), ('sony', 'Sony')], string="Manufacturer")
-    #model_list = {'huawei': [('p20', 'p20'), ('p30', 'p30')]}
-    #model = fields.Selection(model_list.get(manufacturer))
+    name = fields.Char(default="Mobile phone", readonly=False)
+    manufacturer = fields.Selection([('huawei', 'Huawei'), ('xiaomi', 'Xiaomi')])
+    # manufacturer = fields.One2many('product.template', 'model_phone', string="Manufacturer", required=True)
 
-    
+
+class ModelPhone(models.Model):
+    _inherit = 'product.template'
+
+    model_phone = fields.Selection([('p20', 'P20'), ('p20pro', 'P20pro')])
+    # model_phone = fields.Many2one('product_template', string="Model", required=True)
+
 
