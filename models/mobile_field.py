44a3733b75787a0ce76da8d776efe8827a218702
dan480 <sak_2003@mail.ru>
2021-08-23 05:57:24 +0300

    Update mobile_field.py

--- models/mobile_field.py
+++ models/mobile_field.py
@@ -5,15 +5,18 @@ from odoo import fields, models, api
 class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
 
-    name = fields.Char(default="Mobile phone", readonly=False)
+    name = fields.Char(default="Mobile phone", readonly=True)
     manufacturer = fields.Selection([('huawei', 'Huawei'), ('xiaomi', 'Xiaomi')])
     # manufacturer = fields.One2many('product.template', 'model_phone', string="Manufacturer", required=True)
 
-
+    
 class ModelPhone(models.Model):
     _inherit = 'product.template'
 
     model_phone = fields.Selection([('p20', 'P20'), ('p20pro', 'P20pro')])
     # model_phone = fields.Many2one('product_template', string="Model", required=True)
+
+
+
 
 
