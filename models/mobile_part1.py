d53afa328f189d29c1295954845aa79ef8742188
dan480 <sak_2003@mail.ru>
2021-08-25 22:31:13 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -5,13 +5,19 @@ from odoo import fields, models, api
 class CustomProduct(models.Model):
     _inherit = 'product.template'
     
-    product_id = fields.Many2ona('product.mobile')
+    product_id = fields.Many2ona('manufacturer.phone')
+    manufacturer = fields.Selection([('huawei', 'Huawei')])
 
 
-class ProductMobile(models.Model):
-    _name = 'product.mobile'
-    _description = 'Add manufacturer'
+class ManufacturerPhone(models.Model):
+    _name = 'manufacturer.phone'
+    _description = 'Add field manufacturer in product'
 
     manufacturer = fields.Selection([('huawei', 'Huawei')])
 
+class ModelPhone(models.Model):
+    _name = 'model.phone'
+    _description = 'Add field model in product'
+    
+    model_phone = fields.Selection([('p20', 'P20')])
 
