38ad8a9f4004cf2ff5d3877918089c677240ac13
dan480 <sak_2003@mail.ru>
2021-08-25 23:55:24 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -7,12 +7,7 @@ class CustomProduct(models.Model):
     _inherit = 'product.template'
     
     product_id = fields.Many2ona('mobile.phone')
-
-class MobilePhone(models.Model):
-    _name = 'mobile.phone'
-    _inherit = 'custom.product'
-
-    # manufacturer = fields.Selection([('huawei', 'Huawei')])
-    manufacturer = fields.One2many('custom.product', 'product_id')
-    model_phone = fields.Selection([('p20', 'P20')])
+    manufacturer = fields.Selection([('huawei', 'Huawei')])
+    #manufacturer = fields.One2many('custom.product', 'product_id')
+    #model_phone = fields.Selection([('p20', 'P20')])
    
