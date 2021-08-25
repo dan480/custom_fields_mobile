9f55ec6e3b7394fb78a6f217883d7d46979d7c39
dan480 <sak_2003@mail.ru>
2021-08-25 23:48:02 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -11,19 +11,8 @@ class CustomProduct(models.Model):
 class MobilePhone(models.Model):
     _name = 'mobile.phone'
     _inherit = 'custom.product'
-    _description = 'Add custom fields in product'
 
     # manufacturer = fields.Selection([('huawei', 'Huawei')])
     manufacturer = fields.One2many('custom.product', 'product_id')
     model_phone = fields.Selection([('p20', 'P20')])
-    
-    @api.model
-    def create(self, vals):
-        print("Mobile phone create vals ",vals)
-        return super(MobilePhone, self).create(vals)
-
-    def write(self, vals):
-        print("Mobile phone write vals ",vals)
-        return super(MobilePhone, self).write(vals)
-
    
