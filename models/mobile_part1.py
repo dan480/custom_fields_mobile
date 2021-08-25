9771f3df7b90f2593edabae20a7961e2bec66a3e
dan480 <sak_2003@mail.ru>
2021-08-25 21:41:56 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -5,6 +5,6 @@ from odoo import fields, models, api
 class ProductMobile(models.Model):
     _name = 'product.mobile'
 
-    manufacturer = fields.Selection([('huawei', 'Huawei')])
+    #manufacturer = fields.Selection([('huawei', 'Huawei')])
 
 
