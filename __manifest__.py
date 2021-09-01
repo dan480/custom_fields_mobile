d0c15bf9ee746abe192a82a6f394b9b48f13a629
dan480 <sak_2003@mail.ru>
2021-09-02 02:22:35 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name':'Custom_module',
-    'version':'0.1',
-    'author':'A.Sakovich',
-    'summary':"Custom mobile module",
-    'description':"A module that modifies the standard <Product> module",
-    'category':'Customizations',
-    'depends':['product', 'sale'],
+    'name': 'Custom_module',
+    'version': '0.1',
+    'author': 'A.Sakovich',
+    'summary': "Custom mobile module",
+    'description': "A module that modifies the standard <Product> module",
+    'category': 'Customizations',
+    'depends': ['product', 'sale'],
     'data': [
         'views/mobile_views_part1.xml',
         'views/mobile_views_part2.xml',
+        'security/base_security.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': True,
