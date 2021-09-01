1408a335d4aff55ec24eed94624bee72df5a9d31
dan480 <sak_2003@mail.ru>
2021-09-01 16:41:57 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -3,22 +3,25 @@ from odoo import fields, models, api
 
 
 class MobileProduct(models.Model):
-    _inherit = 'product.template'
-    _rec_name = 'name_manufacturer'
+    _name = 'mobile.product'
+    
+    manufacturer_id = fields.Many2one('manufacturer.phone', string="Manufacturer")
+    
+class ManufacturerPhone(models.Model):
+    _name = 'manufacturer.phone'
+    
+    manufacturer_name = fields.Char('Manufacturer')
+    
 
-    name_manufacturer = fields.Char('manufacturer')
-    manufacturer = fields.Many2one('product.template', string="Manufacturer")
-    model = fields.Many2one('product.template', string='Model')
+#     @api.model
+#     def create(self, values):
+#         print("Student create method vals ", values)
+#         rtn = super(MobileProduct, self).create(values)
+#         return rtn
 
-    @api.model
-    def create(self, values):
-        print("Student create method vals ", values)
-        rtn = super(MobileProduct, self).create(values)
-        return rtn
-
-    def write(self, values):
-        print("Student write method vals ", values)
-        rtn = super(MobileProduct, self).write(values)
-        return rtn
+#     def write(self, values):
+#         print("Student write method vals ", values)
+#         rtn = super(MobileProduct, self).write(values)
+#         return rtn
 
    
