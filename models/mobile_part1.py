724bc34dfabd552c5821c0c7e788a900c5f2157c
dan480 <sak_2003@mail.ru>
2021-09-02 17:09:08 +0300

    Update mobile_part1.py

--- models/mobile_part1.py
+++ models/mobile_part1.py
@@ -5,7 +5,7 @@ from odoo import fields, models, api
 class MobileProduct(models.Model):
     _inherit = 'product.template'
 
-    mobile_name = fields.Char(dafault="Mobile phone", readonly=True, compute = add_name)
+    mobile_name = fields.Char(dafault="Mobile phone", readonly=True, compute = _compute_name)
     manufacturer_id = fields.Many2one('manufacturer.phone')
     manufacturer_name = fields.Char(string="Manufacturer", store=True, related="manufacturer_id.name")
     model_id = fields.Many2one('model.phone')
@@ -26,8 +26,10 @@ class MobileProduct(models.Model):
         res['domain']={'model_id':[('manufacturer_id', '=', self.manufacturer_id.id)]}
         return res
     
-    def add_name(self, values):
-        pass
+    @api.depends('manufacturer_name', 'model_name')
+    def add_name(self):
+        for rec in self:
+            rec.mobile_name = "Mobile phone " + record.manufacturer_name + record.model_name
 
 
 class ManufacturerPhone(models.Model):
